01f4d37502f23adc8d426145e49d91e7fc575297
juanda <juandgonzalez12@gmail.com>
2022-10-23 11:34:57 -0500

    finalizacion de la clase dia 14 oct

--- app/src/main/java/com/example/vampire/MainActivity.kt
+++ app/src/main/java/com/example/vampire/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.example.vampire
 
+import android.content.DialogInterface
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import com.google.android.material.textfield.TextInputEditText
 
@@ -16,6 +18,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(findViewById(R.id.my_toolbar))
         edtUsername = findViewById(R.id.edtUsername)
         edtpassword = findViewById(R.id.edtpassword)
     }
@@ -24,26 +27,48 @@ class MainActivity : AppCompatActivity() {
     {
         val massegeusername=getString(R.string.messageusername)
         val massegepassword=getString(R.string.messagepassword)
+        var username: String=edtUsername!!.text.toString()
+        if (username=="juan@correo.com")
+        {
+            if (edtpassword!!.text.toString() == "12345")
+            {
+                val negativeButton={_:DialogInterface,_:Int->}
+                val positiveButton={dialog:DialogInterface,which:Int->
+                    val intent = Intent(this, WelcomeActivity::class.java)
+                    startActivity(intent)
+                }
 
-        if (edtUsername!!.text.toString()=="juan@correo.com") {
-            if (edtpassword!!.text.toString() == "12345") {
-                val intent = Intent(this, WelcomeActivity::class.java)
-                startActivity(intent)
 
-            } else {
+                val dialog= AlertDialog.Builder(this)
+                                                .setTitle("welcome")
+                                                .setMessage("User: "+ username)
+                                                .setPositiveButton("ok", positiveButton)
+                                                .setNegativeButton("Cancelar", negativeButton)
+                                                .create()
+                                                .show()
+
+            } else
+            {
+
                 val dialog = AlertDialog.Builder(this)
                     .setTitle("ERROR")
                     .setMessage(massegepassword)
                     .create()
                     .show()
+
+
             }
 
-        }  else {
+        }  else
+        {
+            /*
             val dialog = AlertDialog.Builder(this)
                 .setTitle("ERROR")
                 .setMessage(massegeusername)
                 .create()
                 .show()
+            */
+            Toast.makeText(this, massegeusername, Toast.LENGTH_LONG).show()
         }
     }
 }
