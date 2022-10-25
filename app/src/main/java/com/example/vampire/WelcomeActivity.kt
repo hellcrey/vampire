22ef7b0117f6ea75760fd5e4469078aede667e81
juanda <juandgonzalez12@gmail.com>
2022-10-25 17:29:47 -0500

    antes de fragment

--- app/src/main/java/com/example/vampire/WelcomeActivity.kt
+++ app/src/main/java/com/example/vampire/WelcomeActivity.kt
@@ -1,11 +1,21 @@
 package com.example.vampire
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.google.android.material.snackbar.Snackbar
 
 class WelcomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
+        setSupportActionBar(findViewById(R.id.my_toolbar))
+
+        val fab: View= findViewById(R.id.fab)
+        fab.setOnClickListener{View->
+            Snackbar.make(View, "power", Snackbar.LENGTH_LONG).show()
+
+        }
     }
 }
