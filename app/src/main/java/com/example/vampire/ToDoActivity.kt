715e0e305261fb64a32b7caaf01a506154059dea
juanda <juandgonzalez12@gmail.com>
2022-10-26 09:19:14 -0500

    ultima actulizacion de la clase del 19 oct

--- app/src/main/java/com/example/vampire/ToDoActivity.kt
+++ app/src/main/java/com/example/vampire/ToDoActivity.kt
@@ -7,5 +7,13 @@ class ToDoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_to_do)
+
+        if(savedInstanceState==null)
+        {
+            supportFragmentManager.beginTransaction()
+                .setReorderingAllowed(true)
+                .add(R.id.fcv,ToDoFragment::class.java, null,"todo")
+                .commit()
+        }
     }
 }
