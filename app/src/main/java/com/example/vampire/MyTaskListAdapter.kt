da2cdc9712b45bb3225590e5a1472f8ceaecbba8
juanda <juandgonzalez12@gmail.com>
2022-11-02 09:13:36 -0500

    finalizacion dia 26 oct corriendo app

---
+++ app/src/main/java/com/example/vampire/MyTaskListAdapter.kt
@@ -0,0 +1,53 @@
+package com.example.vampire
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
+
+class MyTaskListAdapter(context : AppCompatActivity,
+                        val info:Bundle ):RecyclerView.Adapter<MyTaskListAdapter.MyViewHolder>()
+{
+                        class  MyViewHolder(val layout: View):RecyclerView.ViewHolder(layout)
+    private var context :AppCompatActivity=context
+    var myTAskTitles:ArrayList<String> = info.getStringArrayList("titles") as ArrayList<String>
+    var myTAskPlace:ArrayList<String> = info.getStringArrayList("places") as ArrayList<String>
+    var myTAskTime:ArrayList<String> = info.getStringArrayList("times") as ArrayList<String>
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val layout = LayoutInflater.from(parent.context).inflate(R.layout.task_list_item, parent,false)
+        return MyViewHolder(layout)
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        var  textViewTask = holder.layout.findViewById<TextView>(R.id.textViewTask)
+        textViewTask.text=myTAskTitles[position]
+
+        var textViewTime=holder.layout.findViewById<TextView>(R.id.textViewTaskTime)
+        textViewTime.text=myTAskTime[position]
+
+
+
+        holder.layout.setOnClickListener {
+            Toast.makeText(holder.itemView.context,textViewTask.text, Toast.LENGTH_LONG).show()
+            val datos= Bundle()
+            datos.putString("tarea",textViewTask.text as String)
+            datos.putString("hora",textViewTime.text as String)
+            datos.putString("lugar",myTAskPlace[position])
+            context.getSupportFragmentManager()?.beginTransaction()
+                ?.setReorderingAllowed(true)
+                ?.replace(R.id.fcv,DetailFragment::class.java,datos,"detail")
+                ?.addToBackStack("")
+                ?.commit()
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return myTAskTitles.size
+    }
+
+}
--- app/src/main/java/com/example/vampire/ToDoFragment.kt
+++ app/src/main/java/com/example/vampire/ToDoFragment.kt
@@ -4,10 +4,16 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Adapter
 import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.RecyclerView
 
 class ToDoFragment: Fragment() {
+    private  lateinit var listRecyclerView: RecyclerView
+    private lateinit var myAdapter: RecyclerView.Adapter<MyTaskListAdapter.MyViewHolder>
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
@@ -19,7 +25,7 @@ class ToDoFragment: Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val fragmento= inflater.inflate(R.layout.fragment_to_do, container,false)
-        val btnDetail1:Button= fragmento.findViewById(R.id.btn_detail_1)
+ /*       val btnDetail1:Button= fragmento.findViewById(R.id.btn_detail_1)
         val btnDetail2:Button= fragmento.findViewById(R.id.btn_detail_2)
         val btnDetail3:Button= fragmento.findViewById(R.id.btn_detail_3)
 
@@ -35,10 +41,33 @@ class ToDoFragment: Fragment() {
                 ?.replace(R.id.fcv,DetailFragment::class.java,datos,"detail1")
                 ?.addToBackStack("")
                 ?.commit()
-        })
+        })*/
 
 
 
         return fragmento
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        var myTaskTitles: ArrayList<String> = ArrayList()
+        var myTaskTimes: ArrayList<String> = ArrayList()
+        var myTaskPlaces: ArrayList<String> = ArrayList()
+        myTaskTitles.add("ir a super mercado")
+        myTaskTitles.add("llevar el carro a mantenimiento")
+        myTaskTimes.add("10:03 PM")
+        myTaskTimes.add("12:00 M")
+        myTaskPlaces.add("superx")
+        myTaskPlaces.add("tallerx")
+        var info : Bundle=Bundle()
+        info.putStringArrayList("titles", myTaskTitles)
+        info.putStringArrayList("times", myTaskTimes)
+        info.putStringArrayList("places", myTaskPlaces)
+        listRecyclerView = requireView().findViewById(R.id.recyclerToDDoList)
+        myAdapter = MyTaskListAdapter(activity as AppCompatActivity, info)
+        listRecyclerView.setHasFixedSize(true)
+        listRecyclerView.adapter = myAdapter
+        listRecyclerView.addItemDecoration(DividerItemDecoration(context,DividerItemDecoration.VERTICAL))
+    }
+
 }
