7c128fdefe39889eee30990dc03518f33449c124
juanda <juandgonzalez12@gmail.com>
2022-11-06 15:31:48 -0500

    finalizacion clase 9 noviembre

--- app/src/main/java/com/example/vampire/MainActivity.kt
+++ app/src/main/java/com/example/vampire/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.example.vampire
 
+import android.content.Context
 import android.content.DialogInterface
 import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.EditText
+import android.widget.LinearLayout
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
@@ -16,6 +19,7 @@ class MainActivity : AppCompatActivity() {
     //variables
    private var edtUsername: EditText?=null
     private var edtpassword: EditText?=null
+    private var authLayout: LinearLayout?=null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,6 +27,8 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(findViewById(R.id.my_toolbar))
         edtUsername = findViewById(R.id.edtUsername)
         edtpassword = findViewById(R.id.edtpassword)
+        authLayout = findViewById(R.id.authLayout)
+        session()
     }
 
     fun onLogin(botonlogin: View)
@@ -134,4 +140,31 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+    override fun onStart() {
+        super.onStart()
+        authLayout!!.visibility = View.VISIBLE
+    }
+
+    private fun session(){
+        val prefs : SharedPreferences= getSharedPreferences(
+            getString(R.string.prefs_file),
+            Context.MODE_PRIVATE
+        )
+        val email: String? =prefs.getString("email", null)
+        val provider :String? = prefs.getString("provider", null)
+        if (email!=null && provider!= null){
+            authLayout!!.visibility= View.INVISIBLE
+            showHome(email, providerType.valueOf(provider))
+
+        }
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        val prefs = getSharedPreferences(getString(R.string.prefs_file),Context.MODE_PRIVATE).edit()
+        prefs.clear()
+        prefs.apply()
+        FirebaseAuth.getInstance().signOut()
+        onBackPressed()
+    }
 }
