e76fe0f7fb8f65836b99ec9b3308e947c461fe37
juanda <juandgonzalez12@gmail.com>
2022-11-17 21:07:41 -0500

    funcionando boton menu

---
+++ app/src/main/java/com/example/vampire/ListaProductoActivity.kt
@@ -0,0 +1,11 @@
+package com.example.vampire
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ListaProductoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lista_producto)
+    }
+}
